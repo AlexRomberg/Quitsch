6c11bd05f81e5a9430df512a9105daac670aa49b
alexrombi <adze@gmx.ch>
2020-06-20 10:58:47 +0200

    example avatars

---
+++ shop.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Quietscheentenshop</title>
+    <link rel="stylesheet" type="text/css" href="style/style.css" />
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@600&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <header></header>
+    <nav></nav>
+    <main>
+        <div class="product-cards-container">
+            <div class="card"><img src="img_avatar.png" alt="Avatar" style="width:100%">
+                <div class="card-text">
+                    <h4><b>Quietschente silber</b></h4>
+                    <p>Architect & Engineer</p>
+                </div>
+            </div>
+            <div class="card"><img src="img_avatar.png" alt="Avatar" style="width:100%">
+                <div class="card-text">
+                    <h4><b>John Doe</b></h4>
+                    <p>Architect & Engineer</p>
+                </div>
+            </div>
+            <div class="card"><img src="img_avatar.png" alt="Avatar" style="width:100%">
+                <div class="card-text">
+                    <h4><b>John Doe</b></h4>
+                    <p>Architect & Engineer</p>
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer></footer>
+</body>
+
+</html>
